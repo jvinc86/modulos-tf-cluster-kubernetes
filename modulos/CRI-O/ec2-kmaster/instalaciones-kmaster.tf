55261e8964410ff7a3c1762ea51763ad7b3f2020
jvinc86 <jvincent.camacho@gmail.com>
2022-07-10 12:56:00 +0200

    NewChanges

--- modulos/CRI-O/ec2-kmaster/instalaciones-kmaster.tf
+++ modulos/CRI-O/ec2-kmaster/instalaciones-kmaster.tf
@@ -36,12 +36,12 @@ variable "INSTALA_CRI_O" {
     "echo \"[PASO 4] Instalar CRI-O \"",
     "OS=xUbuntu_20.04",
     "VERSION=1.24.1",
-    "echo \"deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /\" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list",
-    "echo \"deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /\" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list",
-    "curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | apt-key add -",
-    "curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key add -",
-    "apt-get update",
-    "apt-get install cri-o cri-o-runc",
+    "echo \"deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /\" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list",
+    "echo \"deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /\" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list",
+    "curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | sudo apt-key add -",
+    "curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key add -",
+    "sudo apt update -y",
+    "sudo apt install -y cri-o cri-o-runc",
 
 
     "echo \"[PASO 5] Copiar archivo que pide Kubernetes llamado 02-cgroup-manager.conf\"",
